9f2abf8798b393256effb84293b3d223739fbd03
Anapocamilo <anapocclnea@gmail.com>
2024-06-09 12:31:49 -0300

    completo

---
+++ desafioIphone/src/classes/AparelhoTelefonico.java
@@ -0,0 +1,25 @@
+package classes;
+
+import interfaces.IAparelhoTelefonico;
+
+public class AparelhoTelefonico implements IAparelhoTelefonico{
+
+    @Override
+    public void ligar(String numero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atender'");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarCorreioVoz'");
+    }
+    
+}
---
+++ desafioIphone/src/classes/ReprodutorMusical.java
@@ -0,0 +1,25 @@
+package classes;
+
+import interfaces.IReprodutorMusical;
+
+public class ReprodutorMusical implements IReprodutorMusical {
+
+    @Override
+    public void tocar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tocar'");
+    }
+
+    @Override
+    public void pausar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
+    }
+    
+}
